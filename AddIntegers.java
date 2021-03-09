6e2f2ffe44b679873c0be9bb007e4851d3168e4d
Samara Sarmiento <ssarmien@umich.edu>
2021-03-09 14:13:46 -0500

    adjusted int first and second

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
+        int first = 55;
+        int second = 65;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
